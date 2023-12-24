2685d709fcf523ae9acc35037f5a293d48b66ef2
Rei3373 <152368897+Rei3373@users.noreply.github.com>
2023-12-25 04:36:14 +0900

    junit03

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -4,6 +4,8 @@ package com.example;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTest {
     // テストをするメソッド全てに@Testを付けます
@@ -15,4 +17,16 @@ class CalculatorTest {
 	    int actual = sut.multiply(5, 6);
 	    assertThat(actual).isEqualTo(expected);
 	}
+	
+	// パラメータ化します
+    @ParameterizedTest
+    // ints = {～}のようにするとパラメータはint型となります
+    // Integer.MAX_VALUEはint型の最大値である2,147,483,647で奇数です
+    @ValueSource(ints = {1, 3, 5, -3, -9, Integer.MAX_VALUE})
+    // intsに定義されている要素数分テストを実行します
+    public void 複数のパラメータが奇数であるかを検証する(int param){
+        boolean actual = sut.isOdd(param);
+        boolean expected = true;
+        assertThat(actual).isEqualTo(expected);
+    }
 }
